535b5cac4270f9bae367b3cccd21b4efb05e5d34
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-04-28 18:02:20 -0700

    adding service worker

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+  ];
+
+self.addEventListener('install', function (e) {
+
+})
